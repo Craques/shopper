f727a1870ef8a7958352d2f537df06f0d779a77d
Charles Muzhanye <charlesmuzhanye@Charless-Mini.lan>
2025-08-21 10:34:31 +0200

    Added gluestack

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
+import "./global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
@@ -16,10 +18,10 @@ function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <GluestackUIProvider mode="light" mode="light"><SafeAreaProvider>
+          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+          <AppContent />
+        </SafeAreaProvider></GluestackUIProvider>
   );
 }
 
